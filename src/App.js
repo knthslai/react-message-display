2d95e02b71a3e3bd4c1bca6f8ec8e159ba2679e7
Kenneth Lai <knthslai@gmail.com>
2018-09-10 18:45:08 -0400

    getting messages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import { Launcher } from 'react-chat-window'
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messageList: {}
+    };
+  }
+  componentDidMount() {
+    this.getMessages()
+  }
+  async getMessages() {
+    try {
+      const messages = await axios.get(`https://gv-text-api.herokuapp.com/api/texts/to`, {
+        headers: {
+          "Access-Control-Allow-Origin": `*`
+        }
+      })
+      console.log(`messages: `, messages.data)
+    } catch (error) { console.error(error); }
+  }
+  // _onMessageWasSent(message) {
+  //   this.setState({
+  //     messageList: [...this.state.messageList, message]
+  //   })
+  // }
+
+  // _sendMessage(text) {
+  //   if (text.length > 0) {
+  //     this.setState({
+  //       messageList: [...this.state.messageList, {
+  //         author: `them`,
+  //         type: `text`,
+  //         data: { text }
+  //       }]
+  //     })
+  //   }
+  // }
   render() {
+    console.log(`checking: `, this.state.messageList)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <React.Fragment>
+        {/* <Launcher
+          agentProfile={{
+            teamName: `react-live-chat`,
+            imageUrl: `https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png`
+          }}
+          onMessageWasSent={this._onMessageWasSent.bind(this)}
+          messageList={this.state.messageList}
+          showEmoji
+        /> */}
+      </React.Fragment>
     );
   }
 }
